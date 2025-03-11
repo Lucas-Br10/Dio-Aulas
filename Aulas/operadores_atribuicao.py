727f4864051e921943ae4d50cca1fecc22d03008
Lucas Xavier do Nascimento Souza <143665539+Lucas-Br10@users.noreply.github.com>
2025-03-11 20:15:28 -0300

    Add files via upload

---
+++ Aulas/operadores_atribuicao.py
@@ -0,0 +1,35 @@
+saldo = 500
+# Atrbuição simples
+print(saldo)
+
+saldo = 200
+print(saldo)
+# Atrbuição simples
+
+saldo += 10
+print(saldo)
+# Atribuição de adição
+
+saldo -= 5
+print(saldo)
+# Atribuição de subtração
+
+saldo //= 2
+print(saldo)
+# Atribuição de divisão inteira
+
+saldo /= 2
+print(saldo)
+# Atribuição de divisão 
+
+saldo *= 10
+print(saldo)
+# Atribuição de multiplicação 
+
+saldo %= 4
+print(saldo)
+# Atribuição de módulo 
+
+saldo **= 2
+print(saldo)
+# Atribuição de exponenciação 
