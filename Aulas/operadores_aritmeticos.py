70823730481d771588c6b4de4bb585642932f3e0
Lucas Xavier do Nascimento Souza <143665539+Lucas-Br10@users.noreply.github.com>
2025-03-11 19:47:34 -0300

    Add files via upload

---
+++ Aulas/operadores_aritmeticos.py
@@ -0,0 +1,24 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)
+""" 
+Shift + alt + Seta para baixo ( duplica a linha onde o cursor está ou as linhas selecionadas)
+"""
+print(produto_1 - produto_2)
+# Subtração
+print(produto_1 / produto_2)
+# Divisão
+print(produto_1 // produto_2)
+# Divisão inteira
+print(produto_1 * produto_2)
+# Multiplicação
+print(produto_1 % produto_2)
+# Modulo
+print(produto_1 ** produto_2)
+# Exponenciação
+
+x = (10 + 5) * 4 
+y = (10 / 2) + (25 * 2) - (2 ** 2) 
+print(x)
+print(y)
