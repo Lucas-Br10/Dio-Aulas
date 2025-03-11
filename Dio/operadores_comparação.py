35afbf0f28e7eb89506ea025078ad3dc6adbb9a3
Lucas Xavier do Nascimento Souza <143665539+Lucas-Br10@users.noreply.github.com>
2025-03-11 19:46:01 -0300

    Add files via upload

---
+++ Dio/operadores_comparação.py
@@ -0,0 +1,15 @@
+saldo = 200
+saque = 200
+
+print(saldo == saque)
+# Igual
+print(saldo != saque)
+# Diferente 
+print(saldo > saque)
+# Maior
+print(saldo >= saque)
+# Maior ou igual
+print(saldo < saque)
+# Menor
+print(saldo <= saque)
+# Menor igual
