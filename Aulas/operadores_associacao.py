f873350f88fc325948a5347e4b76b0ef24589476
Lucas Xavier do Nascimento Souza <143665539+Lucas-Br10@users.noreply.github.com>
2025-03-11 22:27:30 -0300

    Add files via upload

---
+++ Aulas/operadores_associacao.py
@@ -0,0 +1,11 @@
+frutas = ["limao", "uva"]
+curso = "Curso de Python"
+
+
+
+print("maça" in frutas)
+# False (in = está)
+print("limao" not in frutas)
+# False (nor in = não está)
+print("Python" in curso)
+# True
