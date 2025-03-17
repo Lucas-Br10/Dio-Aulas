1aae03ae64b9c23a3cf89fdc92049fd14afcfc81
Lucas Xavier do Nascimento Souza <143665539+Lucas-Br10@users.noreply.github.com>
2025-03-16 23:19:53 -0300

    Add files via upload

---
+++ Aulas/identação_blocos.py
@@ -0,0 +1,8 @@
+def sacar(valor): # : Abertura do bloco  
+    saldo = 500
+
+    if sacar >= valor:
+        print("valor sacado")
+        print("Retire seu dinheiro na boca do caixa!")
+
+sacar(500)
